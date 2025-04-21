61f06c38a1e56f8a6824f5ee2f637f6b659e6d9c
MagdElDinAhmed <magdeldin@aucegypt.edu>
2025-04-21 23:25:27 +0200

    Basic API Implemented
    Also venv removed

--- Phase 5/app.py
+++ Phase 5/app.py
@@ -2,10 +2,61 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
+import numpy as np
+import pandas as pd
 
-# References the file
-app = Flask(__name__)
-columns = []
+class NB:
+
+    def __init__(self):
+        self.Px_y= dict()
+        self.Py= dict()
+        self.cols= None
+
+
+    def fit(self,X,y):
+
+        # first calculate the probability of y equal each value
+        # get the y categoried
+        labels,counts = np.unique(y,return_counts=True)
+
+        m = len(y)
+        for i in range(len(labels)):
+            self.Py[labels[i]]= counts[i]/m
+
+        # calculate the probability of each feature value
+        self.cols = X.columns
+        for col in X.columns: # loop each column
+            self.Px_y[col] = dict()
+
+            feature = X[col]
+            values = np.unique(feature)
+            for value in values:
+                self.Px_y[col][value]=dict()
+                for j in range(len(labels)):
+                    self.Px_y[col][value][labels[j]] = len(X[ X[col] == value][y == labels[j]])/counts[j]
+
+
+
+        return self
+
+
+
+    def predict(self,x_test):
+
+        # get P(Y | X_test)
+        out = []
+        for label,prob in self.Py.items(): # loop over each category proba
+            p=prob
+            for i in range(len(x_test)): # loop over each feature in x_test
+                col = self.cols[i]
+                p=p * self.Px_y[col][x_test[i]][label]
+            out.append((p,label))
+
+        # sort probas
+        sor = sorted(out , key= lambda x: x[0], reverse= True)
+
+
+        return sor[0][1]
 
 def initialize_columns():
     with open('columns.txt') as f:
@@ -32,16 +83,32 @@ def check_data(data):
         valid = False
     return valid
 
+# References the file
+app = Flask(__name__)
+columns = []
+model = NB()
+
 @app.route('/predict', methods = ['POST'])
 def predict():
     data = request.get_json()
     data_local = json.loads(data)
     if check_data(data_local):
-        X = make_list(data_local)
+        x = make_list(data_local)
+        prediction = "Not Diabetic"
+        if model.predict(x) == 1.0:
+            prediction = "Diabetic or Prediabetic"
+
+        return jsonify({'prediction': prediction}),200
     else:
         return jsonify({'error': 'Error in Data'}), 400
 
 
+
+
 if __name__ == "__main__":
+    df = pd.read_csv('Phase 2 data.csv')
+    y = df['Diabetes_binary']
+    X = df.drop(['Diabetes_binary'],axis=1)
+    model.fit(X,y)
     columns = initialize_columns()
     app.run(debug=True)
